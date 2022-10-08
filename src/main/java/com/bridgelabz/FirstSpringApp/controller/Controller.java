93f862c8aef5453bae7e497dda8bed2939d7d683
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-10-08 15:35:02 +0530

    Use post request method

--- src/main/java/com/bridgelabz/FirstSpringApp/controller/Controller.java
+++ src/main/java/com/bridgelabz/FirstSpringApp/controller/Controller.java
@@ -1,22 +1,42 @@
+/**
+ * Spring Boot Application
+ */
 package com.bridgelabz.FirstSpringApp.controller;
 
+import com.bridgelabz.FirstSpringApp.Entity.Data;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
-
+    /**
+     *
+     * Show Welcome message
+     */
     @RequestMapping(value = {"/", "/hello"})
     public String hello() {
         return "Hello from Bridgelabz";
     }
+
+    /**
+     * name parameter pass
+     */
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(@RequestParam String name) {
         return "Hello "+ name + " from Bridgelabz " ;
     }
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    /**
+     * Using path variable for name input
+     *
+     */
+    @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
     public String pathhello(@PathVariable String name) {
         return "Hello "+ name + " from Bridgelabz " ;
+    }
+
+    @PostMapping("/add-details")
+    public String details(@RequestBody Data data){
+        return "Hello "+ data.getFirstname() +" "+ data.getLastname();
     }
 }
