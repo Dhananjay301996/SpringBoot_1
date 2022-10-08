5009884a1024512da5385c6982b7b93c19c70b8a
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-10-08 10:12:05 +0530

    Hello message for mark

--- src/main/java/com/bridgelabz/FirstSpringApp/controller/Controller.java
+++ src/main/java/com/bridgelabz/FirstSpringApp/controller/Controller.java
@@ -1,9 +1,7 @@
 package com.bridgelabz.FirstSpringApp.controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
@@ -11,5 +9,9 @@ public class Controller {
     @RequestMapping(value = {"/", "/hello"})
     public String hello() {
         return "Hello from Bridgelabz";
+    }
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(@RequestParam String name) {
+        return "Hello "+ name + " from Bridgelabz " ;
     }
 }
